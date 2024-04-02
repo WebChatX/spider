96eeec72da6c95fe6972b5ef66929ff66621ce15
liliang <1220795758@qq.com>
2024-04-02 21:13:41 +0800

    feat: SpiderClientOptions类

--- client/src/SpiderClientEngine.js
+++ client/src/SpiderClientEngine.js
@@ -1,3 +1,18 @@
-class SpiderClientEngine {}
+import SpiderClientOptions from "./SpiderClientOptions";
+
+class SpiderClientEngine {
+  constructor() {
+    // 初始化引擎
+    this._initEngine(options);
+  }
+
+  /**
+   *
+   * @param {SpiderClientOptions} options
+   */
+  _initEngine(options) {
+    // this.engine = new WebSocket();
+  }
+}
 
 export default SpiderClientEngine;
---
+++ client/src/SpiderClientOptions.js
@@ -0,0 +1,3 @@
+class SpiderClientOptions {}
+
+export default SpiderClientOptions;
